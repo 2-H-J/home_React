1a46e932c694a2671a76d93eb2c5cc80c4c4b1d9
2-H-J <govl7844@gmail.com>
2025-01-07 10:39:44 +0900

    React-link-Router

---
+++ router-ex/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './page/HomePage';
+import AboutPage from './page/AboutPage';
+import NotFoundPage from './page/NotFoundPage';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <hr/>
+      <Routes>
+        {/* Route가 경로별로 렌더링할 컴포넌트 지정 */}
+        {/* 기본 경로 */}
+        <Route path="/" element={<HomePage/>}></Route>
+        {/* /about 경로 */}
+        <Route path="/about" element={<AboutPage/>}></Route>
+        {/* 없는 경로, 잘못된 경로로 접근했을 때 */}
+        <Route path="*" element={<NotFoundPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
