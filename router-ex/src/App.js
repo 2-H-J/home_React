2147e28588263360d9f2713be90ad2be9e2e3468
2-H-J <govl7844@gmail.com>
2025-01-07 11:39:29 +0900

    Router

--- router-ex/src/App.js
+++ router-ex/src/App.js
@@ -5,6 +5,9 @@ import HomePage from './page/HomePage';
 import AboutPage from './page/AboutPage';
 import NotFoundPage from './page/NotFoundPage';
 import PostDetailPage from './page/PostDetailPage';
+import TeamPage from './page/TeamPage';
+import CompanyPage from './page/CompanyPage';
+import SomeComponent from './page/SomeComponent';
 
 
 function App() {
@@ -16,12 +19,24 @@ function App() {
         {/* Route가 경로별로 렌더링할 컴포넌트 지정 */}
         {/* 기본 경로 */}
         <Route path="/" element={<HomePage/>}></Route>
+
         {/* /about 경로 */}
-        <Route path="/about" element={<AboutPage/>}></Route>
+        <Route path="/about" element={<AboutPage/>}>
+
+        {/* Route안 차원 경로 - 하위 Route는 /를 제외 */}
+          <Route path='team' element={<TeamPage/>}></Route>
+          <Route path='company' element={<CompanyPage/>}></Route>
+          
+        </Route>
+
         {/* 없는 경로, 잘못된 경로로 접근했을 때 */}
         <Route path="*" element={<NotFoundPage/>}></Route>
+
         {/* 게시글 상세 페이지 지정 - /post/:postId : 내가 보낸 데이터의 파라미터값, postId PostDetailPage의 변수명이랑 같아야 한다 */}
         <Route path="/post/:postId" element={<PostDetailPage/>}></Route>
+
+        {/* 컴포넌트 이동 */}
+        <Route path='/some' element={<SomeComponent/>}></Route>
       </Routes>
     </BrowserRouter>
   );
